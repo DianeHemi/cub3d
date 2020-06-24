dd16125199f023bd5ef8d22f2044c6f0293904ab
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-06-24 11:53:54 +0200

    Ajout libft et headers

---
+++ includes/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/13 12:20:56 by dchampda          #+#    #+#             */
+/*   Updated: 2020/05/13 12:20:58 by dchampda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+int		find_n(char *buffer, char c);
+size_t	ft_strlen(char const *str);
+char	*ft_strndup(char *str, int len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*copy_after_n(char *str, int len);
+
+#endif

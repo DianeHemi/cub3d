811481d14128c29441352594efa8723c443b53c1
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-06-25 16:42:59 +0200

    Starting to build the hader

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,14 +6,36 @@
 /*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 11:46:45 by dchampda          #+#    #+#             */
-/*   Updated: 2020/06/24 11:47:10 by dchampda         ###   ########.fr       */
+/*   Updated: 2020/06/25 16:31:09 by dchampda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 #define CUB3D_H
 
+#include <math.h>
 
+
+/*
+ *** Structure generale, contenant autres structs
+*/
+typedef	s_struct
+{
+	t_map	map;
+
+
+
+}		t_cub
+
+
+/*
+ *** Structure pour les infos de la map 
+*/
+typedef	s_map
+{
+
+
+}		t_map
 
 
 #endif

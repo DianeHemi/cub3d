a86dfd59247b22601a0fce0dc1ce73263a959db1
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-06-28 21:24:15 +0200

    Modifications

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,14 +6,13 @@
 /*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 11:45:57 by dchampda          #+#    #+#             */
-/*   Updated: 2020/06/25 16:42:03 by dchampda         ###   ########.fr       */
+/*   Updated: 2020/06/24 11:46:28 by dchampda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
-#include "../libft/libft.h"
 
-int	ft_check_args(int argc, char **argv)
+int	ft_check_args(int argc, char **argv, int *save_option)
 {
 	if (argc < 2 || argc > 3)
 	{
@@ -22,40 +21,42 @@ int	ft_check_args(int argc, char **argv)
 	}
 	if (argc == 3)
 	{
-		if (ft_strncmp(argv[2], "--save") != 0)
+		if (ft_strncmp(argv[2], "--save") != 0 
+			|| ft_strchr(argv[1], ".cub") == XX)
 		{
-			write(2, "Error : --save to save screen\n", 31);
+			write(2, "Error : invalid arguments\n", 26);
 			return (0);
 		}
 		else
 		{
-			//Lancement fonction de sauvegarde
-			//return (1);
+			//Lancement fonction de sauvegarde ?
+			*save_option = 1;
+			return (1);
 		}
 	}
 	else
 	{
-		if (ft_strchr(argv[1], ".cub") == XX) //Fonction pour trouver mot dans phrase
+		if (ft_strchr(argv[1], ".cub") == XX) //Fonction pour trouver mot dans phrase a déplacer dans parsing
 		{
 			write(2, "Invalid format : Select a .cub map\n", 35);
 			return (0);
 		}
 		else
-			return (1);  //Tout est bon, on passe au parsing de la map
+			return (1);
 	}
 }
 
 int	main(int argc, char **argv)
 {
+	int	save_option;
 
-
-
-	if (!ft_check_args(argc, argv))
-		return (-1);	//Affichage des erreurs sur les arguments
+	save_option = 0;
+	if (!ft_check_args(argc, argv, &save_option))
+		return (EXIT_FAILURE);	//Affichage des erreurs sur les arguments
 
 
 	if (!ft_parsing(argv[1]))
-		return (-1);    //Affichage des erreurs sur fichier et parsing sera gere dans ft_parsing
+		return (EXIT_FAILURE);    //Affichage des erreurs sur fichier et parsing sera gere dans ft_parsing
 
 
 

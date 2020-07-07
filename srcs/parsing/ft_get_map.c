827aaba63f5ad9335846238036be64cd0f6bb46a
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-07-07 18:01:24 +0200

    Leak correction

--- srcs/parsing/ft_get_map.c
+++ srcs/parsing/ft_get_map.c
@@ -6,7 +6,7 @@
 /*   By: dchampda <dchampda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 19:05:03 by dchampda          #+#    #+#             */
-/*   Updated: 2020/07/07 17:51:16 by dchampda         ###   ########.fr       */
+/*   Updated: 2020/07/07 18:00:46 by dchampda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	ft_get_map(int fd, char *line, t_config *config)
 {
 	int		read;
 	char	*map_tmp;
+	char	*tmp;
 	int		i;
 
 	i = 0;
@@ -30,7 +31,9 @@ int	ft_get_map(int fd, char *line, t_config *config)
 	//Lire les lignes 1 par 1 et les copier ?
 	while ((read = get_next_line(fd, &line)) > 0)
 	{
+		tmp = map_tmp;
 		map_tmp = ft_strjoin2(map_tmp, line);
+		free(tmp);
 		free(line);		
 	}
 	

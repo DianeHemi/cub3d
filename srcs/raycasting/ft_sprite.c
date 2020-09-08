04fd50356f1063a1e41b101aedacc34a6d3239ab
Diane <64610122+DianeHemi@users.noreply.github.com>
2020-09-08 18:05:11 +0200

    Corrected deformations with weird ratios

--- srcs/raycasting/ft_sprite.c
+++ srcs/raycasting/ft_sprite.c
@@ -12,54 +12,6 @@
 
 #include "../../includes/cub3d.h"
 
-
-/*--------------------------------------------*/
-/*--------------------------------------------*/
-/*--------------------------------------------*/
-
-void 	ft_swap_sprites(t_sprite *sprite1, t_sprite *sprite2)
-{
-	t_sprite tmp;
-
-	tmp = *sprite1;
-	*sprite1 = *sprite2;
-	*sprite2 = tmp;
-}
-
-double		ft_sprite_dist(t_ray *ray, t_sprite *sprite)
-{
-	double sprite_dist;
-
-	sprite_dist = ((ray->pos_x - sprite->x) * (ray->pos_x - sprite->x) 
-			+ (ray->pos_y - sprite->y) * (ray->pos_y - sprite->y));
-	return (sprite_dist);
-}
-
-void		ft_sort_sprites(t_game *game, int nb_sprite)
-{
-	int i;
-	int j;
-
-	i = 1;
-	while (i < nb_sprite)
-	{
-		j = i;
-		while (j > 0 && ft_sprite_dist(game->ray, &game->sprite[j - 1]) < ft_sprite_dist(game->ray, &game->sprite[j]))
-		{
-			ft_swap_sprites(&game->sprite[j], &game->sprite[j - 1]);
-			j--;
-		}
-		i++;
-	}
-}
-
-
-/*--------------------------------------------*/
-/*--------------------------------------------*/
-/*--------------------------------------------*/
-
-
-
 void 	ft_draw_sprite(t_config *config, t_sprite_data *s_data, t_sprite *sprite, t_game *game)
 {
 	s_data->x = sprite->x - game->ray->pos_x;
@@ -69,7 +21,7 @@ void 	ft_draw_sprite(t_config *config, t_sprite_data *s_data, t_sprite *sprite, 
 	s_data->transformY = s_data->invDet * (-game->ray->plane_y * s_data->x + game->ray->plane_x * s_data->y);
 	s_data->screen_x = (int)((config->width / 2) * (1 + s_data->transformX / s_data->transformY));
 	//Calculate height of sprite on screen
-	s_data->s_height = abs((int)(config->height / (s_data->transformY)));
+	s_data->s_height = (int)(((config->width / 2) / tan(30 * (M_PI/180))) / (s_data->transformY));
 	//get stripe size
 	s_data->drawStart_y = -s_data->s_height / 2 + config->height / 2;
 	if (s_data->drawStart_y < 0)
@@ -78,7 +30,7 @@ void 	ft_draw_sprite(t_config *config, t_sprite_data *s_data, t_sprite *sprite, 
 	if (s_data->drawEnd_y >= config->height)
 		s_data->drawEnd_y = config->height - 1;
 	//Calcul de la width du sprite
-	s_data->s_width = abs((int)(config->height / (s_data->transformY)));
+	s_data->s_width = (int)(((config->width / 2) / tan(30 * (M_PI/180))) / (s_data->transformY));
 	s_data->drawStart_x = -s_data->s_width / 2 + s_data->screen_x;
 	if (s_data->drawStart_x < 0)
 		s_data->drawStart_x = 0;
